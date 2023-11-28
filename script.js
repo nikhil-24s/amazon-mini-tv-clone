da28e206110b8bc511722ae50c3d1f99250663a0
nikhil-24s <1234nikhilsolanki@gmail.com>
2023-11-27 20:03:02 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+function scrollRight(){
+    document.getElementById('thumbnail').scrollLeft += 700;
+    document.getElementById('left-btn').style.display= 'inline';
+    
+}
+function scrollLeft1(){
+    document.getElementById('thumbnail').scrollLeft -= 700;
+    if(document.getElementById('thumbnail').scrollLeft <= 700)
+    {
+        document.getElementById('left-btn').style.display= 'none';
+    }
+}
+
+function scrollRight1(){
+    document.getElementById('thumbnail1').scrollLeft += 700;
+    document.getElementById('left-btn1').style.display= 'inline';
+    
+}
+function scrollLeft11(){
+    document.getElementById('thumbnail1').scrollLeft -= 700;
+    if(document.getElementById('thumbnail1').scrollLeft <= 700)
+    {
+        document.getElementById('left-btn1').style.display= 'none';
+    }
+}
+
+
+function scrollRight11(){
+    document.getElementById('thumbnail11').scrollLeft += 700;
+    document.getElementById('left-btn11').style.display= 'inline';
+    
+}
+function scrollLeft111(){
+    document.getElementById('thumbnail11').scrollLeft -= 700;
+    if(document.getElementById('thumbnail11').scrollLeft <= 700)
+    {
+        document.getElementById('left-btn11').style.display= 'none';
+    }
+}
+
+function scrollRight12(){
+    document.getElementById('thumbnail12').scrollLeft += 700;
+    document.getElementById('left-btn12').style.display= 'inline';
+    
+}
+function scrollLeft112(){
+    document.getElementById('thumbnail12').scrollLeft -= 700;
+    if(document.getElementById('thumbnail12').scrollLeft <= 700)
+    {
+        document.getElementById('left-btn12').style.display= 'none';
+    }
+}
